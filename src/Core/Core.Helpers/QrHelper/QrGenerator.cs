efd2d020a77fd6aa00912e9cfc15dfdc1c058b7b
Ahmet ÇAKIR <ahmetcaakirr@gmail.com>
2024-08-25 23:01:26 +0300

    qrgenerator

---
+++ src/Core/Core.Helpers/QrHelper/QrGenerator.cs
@@ -0,0 +1,17 @@
+﻿using QRCoder;
+
+namespace Core.Helpers.QrHelper;
+public static class QrGenerator
+{
+    public static Task<byte[]> GenerateQrCodeAsync(string text)
+    {
+        var qrGenerator = new QRCodeGenerator();
+        var qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+
+        var qrCode = new PngByteQRCode(qrCodeData);
+
+        var qrCodeImage = qrCode.GetGraphic(20);
+
+        return Task.FromResult(qrCodeImage);
+    }
+}
